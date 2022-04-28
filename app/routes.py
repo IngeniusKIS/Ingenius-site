3c6027c56da3b6f2fc5db0b77f444ee21e0c20c2
sikrypt-team <sikrypt@gmail.com>
2022-04-28 21:07:30 +0530

    about and events hyperlink added

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,12 @@ def home():
 @app.route('/register/')
 def register():
 	return render_template('register.html')
+
+
+@app.route('/about/')
+def about():
+	return redirect('https://fast-falls-22011.herokuapp.com/#about')
+
+@app.route('/events/')
+def events():
+	return redirect('https://fast-falls-22011.herokuapp.com/#events')
